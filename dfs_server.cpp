9263011eb49a692a03552c027c1a7c96a4e6228d
Roy Chan <nerdroychan@users.noreply.github.com>
2019-07-13 19:25:00 +0800

    Update

--- dfs_server.cpp
+++ dfs_server.cpp
@@ -1,8 +1,6 @@
-#include <algorithm>
-#include <chrono>
-#include <cmath>
-#include <iostream>
-#include <memory>
+#include <unistd.h>
+
+
 #include <string>
 
 #include <grpc/grpc.h>
@@ -13,10 +11,49 @@
 
 #include "protos/dfs.grpc.pb.h"
 
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::ServerReader;
+using grpc::ServerReaderWriter;
+using grpc::ServerWriter;
+using grpc::Status;
+
+using dfs::Void;
+using dfs::Str;
+using dfs::Bool;
+using dfs::Int;
+using dfs::Dentry;
+using dfs::Block;
+using dfs::WriteRequest;
+using dfs::ReadResponse;
+using dfs::RandomReadRequest;
+
 
 class DFSImpl final : public DFS::Service {
+    Status get_dir(ServerContext* context, const Void* req, Str* res) {
+        char* cwd = getcwd();
+        std::string cwd_string(cwd);
+        res->content = cwd_string;
 
+        return Status::OK;
+    }
 }
 
 
+void run_server() {
+  std::string server_address("0.0.0.0:50051");
+  DFSImpl service();
 
+  ServerBuilder builder;
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  builder.RegisterService(&service);
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+  server->Wait();
+}
+
+int main(int argc, char** argv) {
+    run_server();
+    return 0;
+}
