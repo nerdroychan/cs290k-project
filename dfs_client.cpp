b91d85e407dfec2cd61d4bdb0203a9e2127458f5
Chen Chen <mail@roychan.org>
2019-07-14 16:46:40 +0800

    Update client and server

--- dfs_client.cpp
+++ dfs_client.cpp
@@ -1,3 +1,11 @@
+#include <unistd.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
 #include <iostream>
 #include <vector>
 
@@ -38,10 +46,10 @@ public:
         Str res;
         Status status = this->_stub->get_dir(&context, req, &res);
         if (!status.ok()) {
-            std::cout << "get_cwd failed" << std::endl;
-        } else {
-            std::cout << "get_cwd succeed, returned " << res.content() << std::endl;
+            std::cout << "getdir failed due to get_dir failure" << std::endl;
+            return;
         }
+        std::cout << "getdir succeed, returned " << res.content() << std::endl;
     }
 
     void cd(const string path) {
@@ -51,11 +59,12 @@ public:
         req.set_content(path);
         Status status = this->_stub->change_dir(&context, req, &res);
         if (!status.ok() || res.value() == false) {
-            std::cout << "change_dir failed" << std::endl;
-        } else {
-            std::cout << "change_dir succeed, returned " << res.value() \
+            std::cout << "changedir failed due to change_dir failure" \
                 << std::endl;
+            return;
         }
+        std::cout << "change_dir succeed, returned " << res.value() \
+                << std::endl;
     }
 
     void filecount() {
@@ -64,11 +73,12 @@ public:
         Int res;
         Status status = this->_stub->file_count(&context, req, &res);
         if (!status.ok() || res.value() == -1) {
-            std::cout << "file_count failed" << std::endl;
-        } else {
-            std::cout << "file_count succeed, returned " << res.value() \
+            std::cout << "filecount failed due to file_count failure" \
                 << std::endl;
-        }
+            return;
+        } 
+        std::cout << "file_count succeed, returned " << res.value() \
+            << std::endl;
     }
 
     void ls(bool list, const string path) {
@@ -120,8 +130,9 @@ public:
             std::cout << it->name() << " ";
             if (list) {
                 std::cout << it->is_directory() << " " << it->size() << " " \
-                    << it->last_modified_time() << " " << std::endl;
+                    << it->last_modified_time() << " "; 
             }
+            std::cout << std::endl;
         }
     } 
 
@@ -129,31 +140,239 @@ public:
         ClientContext context;
         Void req;
         Str res;
+
         Status status = this->_stub->get_dir(&context, req, &res);
         if (!status.ok()) {
             std::cout << "ls failed due to get_dir failure" <<std::endl;
-        } else {
-            ls(list, res.content());
+            return;
         }
+        ls(list, res.content());
+    }
+
+    void ls() {
+        this->ls(false);
     }
 
     void put(const string localpath, const string remotepath) {
+        mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+        int fd = open(localpath.c_str(), O_RDONLY, mode);
+        if (fd == -1) {
+            std::cout << "put failed because cannot open local file" \
+                << std::endl;
+            return;
+        }
+
+        // invoke open_file_to_write
+        ClientContext open_file_to_write_context;
+        Str open_file_to_write_req;
+        Bool open_file_to_write_res;
+
+        open_file_to_write_req.set_content(remotepath);
+        Status open_file_to_write_status = this->_stub->open_file_to_write(
+                &open_file_to_write_context, open_file_to_write_req, \
+                &open_file_to_write_res
+                );
+        if (!open_file_to_write_status.ok() \
+                || !open_file_to_write_res.value()) {
+            std::cout << "put failed due to open_file_to_write failure" \
+                << std::endl;
+            close(fd);
+            return;
+        }
+
+        // invoke next_write
+        WriteRequest next_write_req;
+        Bool next_write_res;
+        char buf[512];
+        int bytes_read = 0;
+        while (bytes_read >= 0) {
+            bytes_read = read(fd, buf, 512);
+            if (bytes_read == -1) {
+                std::cout << "put failed because cannot read local file" \
+                    << std::endl;
+                close(fd);
+                return;
+            } else if (bytes_read == 0) break;
+            next_write_req.mutable_block()->set_content(buf);
+            next_write_req.set_size(bytes_read);
+            ClientContext next_write_context;
+
+            Status next_write_status = this->_stub->next_write(
+                    &next_write_context, next_write_req, &next_write_res
+                    );
+            if (!next_write_status.ok() || !next_write_res.value()) {
+                std::cout << "put failed due to next_write failure" \
+                    << std::endl;
+                close(fd);
+                return;
+            }
+        }
+
+        // invoke close_file
+        ClientContext close_file_context;
+        Void close_file_req;
+        Bool close_file_res;
+
+        Status close_file_status = this->_stub->close_file(
+                &close_file_context, close_file_req, &close_file_res
+                );
+        if (!close_file_status.ok() || !close_file_res.value()) {
+            std::cout << "put failed due to close_file failure" << std::endl;
+            close(fd);
+            return;
+        }
+
+        std::cout << "put succeed" << std::endl;
+        close(fd);
     }
 
     void put(const string localpath) {
+        string remotepath(localpath);
+        put(localpath, remotepath);
     }
 
     void get(const string remotepath, const string localpath) {
+        mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+        int fd = open(localpath.c_str(), O_CREAT | O_WRONLY, mode);
+        if (fd == -1) {
+            std::cout << localpath.c_str() << " ";
+            std::cout << "get failed because cannot open local file" \
+                << std::endl;
+            return;
+        }
+
+        // invoke open_file_to_read
+        ClientContext open_file_to_read_context;
+        Str open_file_to_read_req;
+        Bool open_file_to_read_res;
+
+        open_file_to_read_req.set_content(remotepath);
+        Status open_file_to_read_status = this->_stub->open_file_to_read(
+                &open_file_to_read_context, open_file_to_read_req, \
+                &open_file_to_read_res
+                );
+        if (!open_file_to_read_status.ok() \
+                || !open_file_to_read_res.value()) {
+            std::cout << "get failed due to open_file_to_read failure" \
+                << std::endl;
+            close(fd);
+            return;
+        }
+
+        // invoke next_read
+        Void next_read_req;
+        ReadResponse next_read_res;
+
+        int bytes_read = 0;
+        while (bytes_read >= 0) {
+            ClientContext next_read_context;
+            Status next_read_status = this->_stub->next_read(
+                    &next_read_context, next_read_req, &next_read_res
+                    );
+            if (!next_read_status.ok() || next_read_res.size() == -1) {
+                std::cout << "get failed due to next_read failure" << std::endl;
+                close(fd);
+                return;
+            }
+            bytes_read = next_read_res.size();
+            if (bytes_read == 0) break;
+            int ret = write(fd, next_read_res.block().content().c_str(), \
+                   bytes_read);
+            if (ret == -1) {
+                std::cout << "get failed because cannot write local file" \
+                    << std::endl;
+                close(fd);
+                return;
+            }
+        }
+
+        // invoke close_file
+        ClientContext close_file_context;
+        Void close_file_req;
+        Bool close_file_res;
+
+        Status close_file_status = this->_stub->close_file(
+                &close_file_context, close_file_req, &close_file_res
+                );
+        if (!close_file_status.ok() || !close_file_res.value()) {
+            std::cout << "get failed due to close_file failure" << std::endl;
+            close(fd);
+            return;
+        }
+        
+        std::cout << "get succeed" << std::endl;
+        close(fd);
     }
 
     void get(const string remotepath) {
+        string localpath(remotepath);
+        get(remotepath, localpath);
     }
 
     void randomread(const string remotepath, int firstbyte, int numbytes) {
+        if (numbytes > 512) {
+            std::cout << "randomread failed because size over 512" << std::endl;
+            return;
+        }
+        // invoke open_file_to_read
+        ClientContext open_file_to_read_context;
+        Str open_file_to_read_req;
+        Bool open_file_to_read_res;
+
+        open_file_to_read_req.set_content(remotepath);
+        Status open_file_to_read_status = this->_stub->open_file_to_read(
+                &open_file_to_read_context, open_file_to_read_req, \
+                &open_file_to_read_res
+                );
+        if (!open_file_to_read_status.ok() \
+                || !open_file_to_read_res.value()) {
+            std::cout << "randomread failed due to open_file_to_read failure" \
+                << std::endl;
+            return;
+        }
+
+        // invoke random_read
+        ClientContext random_read_context;
+        RandomReadRequest random_read_req;
+        ReadResponse random_read_res;
+
+        random_read_req.set_offset(firstbyte);
+        random_read_req.set_size(numbytes);
+
+        Status random_read_status = this->_stub->random_read(
+                &random_read_context, random_read_req, &random_read_res);
+
+        if (!random_read_status.ok() || random_read_res.size() == -1) {
+            std::cout << "randomread failed due to random_read failure" \
+                << std::endl;
+            return;
+        }
+
+        // invoke close_file
+        ClientContext close_file_context;
+        Void close_file_req;
+        Bool close_file_res;
+
+        Status close_file_status = this->_stub->close_file(
+                &close_file_context, close_file_req, &close_file_res
+                );
+        if (!close_file_status.ok() || !close_file_res.value()) {
+            std::cout << "randomread failed due to close_file failure" << std::endl;
+            return;
+        }
+
+        char buf[513];
+        strncpy(buf, random_read_res.block().content().c_str(), \
+                random_read_res.size());
+        buf[random_read_res.size()+1] = '\0';
+        std::cout << buf << std::endl;
+        
+        std::cout << "get succeed" << std::endl;
     }
 
     void shell() {
-        this->ls(true);
+        this->get("./test/remote", "./test/local");
+        this->randomread("./test/remote", 1, 1);
     }
 
 private:
